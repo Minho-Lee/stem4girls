3f4db9f46e4939cc0389e330bbb11e57bc201cbb
Minho Lee <minho.lee.93@hotmail.com>
2017-06-04 16:01:00 -0400

    update Timer, add weeks, balance, user input

--- public/js/main.js
+++ public/js/main.js
@@ -28,8 +28,15 @@ function initializeClock(id, endtime) {
     minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
     secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
 
-    if (t.total <= 0) {
+    if (t.total < 0) {
       clearInterval(timeinterval);
+      var weeks = document.getElementById('weekdiv');
+      var weeksSpan = weeks.querySelector('.weeks');
+      weekCounter += 1;
+      balanceUpdate(salaryValue);
+      weeksSpan.innerHTML = weekCounter;
+      var reset = new Date(Date.parse(new Date()) + 0.00006 * 24 * 60 * 60 * 1000);
+      initializeClock('clockdiv', reset);
     }
   }
 
@@ -37,5 +44,35 @@ function initializeClock(id, endtime) {
   var timeinterval = setInterval(updateClock, 1000);
 }
 
-var deadline = new Date(Date.parse(new Date()) + 0.002 * 24 * 60 * 60 * 1000);
-initializeClock('clockdiv', deadline);
+function submitSalary(){
+  var salary = document.getElementById('salary');
+  //var salaryShow = document.getElementById('salaryShow');
+  if (isNaN(salary.value)) {
+    $('#afterSubmit').html('<h3>Invalid Entry! Please Enter A Number!</h3>');
+  } else {
+    var regexp = /^\d+\.?\d{0,2}$/;
+    if (regexp.test(salary.value) === true) {
+      salary.disabled = true;
+      salaryValue = salary.value;
+      $('#afterSubmit').html('<h3>Your bi-weekly salary is : $' + salaryValue + '</h3>');
+      $('#salaryIncrease').html('<h3>You currently have: $0</h3>');
+      //balanceUpdate(salaryValue);
+      salary.value = "Entered!";
+      var deadline = new Date(Date.parse(new Date()) + 0.00006 * 24 * 60 * 60 * 1000);
+      initializeClock('clockdiv', deadline);  
+    } else {
+      $('#afterSubmit').html('<h3>Invalid Entry! Please Enter A Valid Number! (XXX.XX format)</h3>');
+    }
+    
+  }
+  //console.log($("#salary").val()); 
+}
+
+function balanceUpdate(moneyVal) {
+  balance += parseFloat(moneyVal);
+  $('#salaryIncrease').html('<h3>You currently have: $' + Math.round(parseFloat(balance)*100) / 100 + '</h3>');
+}
+
+var balance = 0;
+var salaryValue = 0;
+var weekCounter = 0;
