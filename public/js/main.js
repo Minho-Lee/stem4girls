0344a3571f8c3e263fd68d56daaa14047178d6de
Minho Lee <minho.lee.93@hotmail.com>
2017-06-08 10:20:30 -0400

    Adding friend list, food expense, house expense, set of events

--- public/js/main.js
+++ public/js/main.js
@@ -21,7 +21,7 @@ function initializeClock(id, endtime) {
   var secondsSpan = clock.querySelector('.seconds');
 
   function updateClock() {
-    var t = getTimeRemaining(endtime);
+    t = getTimeRemaining(endtime);
 
     daysSpan.innerHTML = t.days;
     hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
@@ -32,11 +32,18 @@ function initializeClock(id, endtime) {
       clearInterval(timeinterval);
       var weeks = document.getElementById('weekdiv');
       var weeksSpan = weeks.querySelector('.weeks');
-      weekCounter += 1;
-      balanceUpdate(salaryValue);
+      weekCounter += 2;
+      balanceUpdate('add', salaryValue);
+      if (foodCounter === true) {
+        balanceUpdate('minus', foodCost);
+      }
+      if (houseCounter === true) {
+        balanceUpdate('minus', houseCost);
+      }
+      currentBalance.html('<h2>You currently have: $' + Math.round(parseFloat(balance)*100) / 100 + '</h2>');
       weeksSpan.innerHTML = weekCounter;
-      var reset = new Date(Date.parse(new Date()) + 0.00006 * 24 * 60 * 60 * 1000);
-      initializeClock('clockdiv', reset);
+      deadline = new Date(Date.parse(new Date()) + 0.00006 * 24 * 60 * 60 * 1000);
+      initializeClock('clockdiv', deadline);
     }
   }
 
@@ -46,19 +53,21 @@ function initializeClock(id, endtime) {
 
 function submitSalary(){
   var salary = document.getElementById('salary');
-  //var salaryShow = document.getElementById('salaryShow');
+  var submitButton = document.getElementById('submitSalary');
+  var hideFood = document.getElementById('hideFood');
   if (isNaN(salary.value)) {
     $('#afterSubmit').html('<h3>Invalid Entry! Please Enter A Number!</h3>');
   } else {
-    var regexp = /^\d+\.?\d{0,2}$/;
     if (regexp.test(salary.value) === true) {
+      submitButton.disabled = true;
+      submitButton.innerHTML = "Submitted!"
+      hideFood.style.visibility = "visible";
       salary.disabled = true;
       salaryValue = salary.value;
       $('#afterSubmit').html('<h3>Your bi-weekly salary is : $' + salaryValue + '</h3>');
-      $('#salaryIncrease').html('<h3>You currently have: $0</h3>');
-      //balanceUpdate(salaryValue);
+      currentBalance.html('<h2>You currently have: $0</h2>');
       salary.value = "Entered!";
-      var deadline = new Date(Date.parse(new Date()) + 0.00006 * 24 * 60 * 60 * 1000);
+      deadline = new Date(Date.parse(new Date()) + 0.00006 * 24 * 60 * 60 * 1000);
       initializeClock('clockdiv', deadline);  
     } else {
       $('#afterSubmit').html('<h3>Invalid Entry! Please Enter A Valid Number! (XXX.XX format)</h3>');
@@ -68,11 +77,82 @@ function submitSalary(){
   //console.log($("#salary").val()); 
 }
 
-function balanceUpdate(moneyVal) {
-  balance += parseFloat(moneyVal);
-  $('#salaryIncrease').html('<h3>You currently have: $' + Math.round(parseFloat(balance)*100) / 100 + '</h3>');
+function submitFood() {
+  foodCost = $('#foodCost').val();
+  foodSubmit = $('#foodSubmitButton');
+  if (isNaN(foodCost)) {
+    $('#afterFoodSubmit').html('<h4>Invalid Entry! Please Enter A Number!</h4>');
+    foodCounter = false;
+  } else {
+    $('#afterFoodSubmit').html('<h4>Submitted!</h4>');
+    if (regexp.test(foodCost) === true) {
+      foodCounter = true;
+      foodExpense.html('<h3>Food Expense: $' + foodCost+ '</h3>');
+    } else {
+      $('#afterFoodSubmit').html('<h4>Invalid Entry! Please Enter A Valid Number! (XXX.XX format)</h4>');
+      foodCounter = false;
+    }
+  }
 }
 
+function submitHouse() {
+  houseCost = $('#houseCost').val();
+  houseSubmit = $('#houseSubmitButton');
+  if (isNaN(houseCost)) {
+    $('#afterHouseSubmit').html('<h4>Invalid Entry! Please Enter A Number!</h4>');
+    houseCounter = false;
+  } else {
+    $('#afterHouseSubmit').html('<h4>Submitted!</h4>');
+    if (regexp.test(houseCost) === true) {
+      houseCounter = true;
+      houseExpense.html('<h3>House Expense: $' + houseCost+ '</h3>');
+    } else {
+      $('#afterHouseSubmit').html('<h4>Invalid Entry! Please Enter A Valid Number! (XXX.XX format)</h4>');
+      houseCounter = false;
+    }
+  }
+
+}
+
+function balanceUpdate(action, moneyVal) {
+  if (action === 'add') {
+    balance += Math.round(parseFloat(moneyVal)*100) / 100;
+  } else if (action === 'minus') {
+    balance -= Math.round(parseFloat(moneyVal)*100) / 100;
+  }
+}
+
+function optionSubmit() {
+  var options = $('#options');
+  if (options.val() === 'option1') {
+    balanceUpdate('minus', 200);
+    eventUpdate.html('<h3>You bought brand new shoes! Spent $200.</h3>');
+  } else if(options.val() === 'option2') { 
+    salaryValue = salaryValue * 0.8;
+    eventUpdate.html('<h3>You changed jobs! Decrease salary by 20%.</h3>');
+    $('#afterSubmit').html('<h3>Your bi-weekly salary is : $' + Math.round(parseFloat(salaryValue)*100) / 100 + '</h3>');
+  } else if(options.val() === 'option3') {
+    balanceUpdate('minus', 500);
+    eventUpdate.html('<h3>You bought a brand new phone! Spent $500</h3>');
+  } else if(options.val() === 'option4') {
+    balanceUpdate('add', 1000);
+    eventUpdate.html('<h3>You won a lottery! Gained $1000</h3>');
+  } else if(options.val() === 'option5') {
+    eventUpdate.html('<h3>Nothing happened</h3>');
+    //do nothing
+  }
+  currentBalance.html('<h2>You currently have: $' + Math.round(parseFloat(balance)*100) / 100 + '</h2>');
+}
+
+var currentBalance = $('#currentBalance');
+var foodExpense = $('#foodExpense');
+var houseExpense = $('#houseExpense');
+var eventUpdate = $('#eventUpdate');
+var houseCounter = false, foodCounter = false;
+var t = 0;
+var regexp = /^\d+\.?\d{0,2}$/;
+var foodCost, houseCost = 0;
 var balance = 0;
 var salaryValue = 0;
 var weekCounter = 0;
+var deadline = 0;
